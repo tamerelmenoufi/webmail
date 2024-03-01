d8eaf57a561415fa0ab32fa48269f4825548dd2d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-01 22:16:03 +0200

    Atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include("../../sis/MysqlConnect.php");
+    // include("../../sis/MysqlConnect.php");
     include("fn.php");
     // $con = AppConnect('api_mailgun');
     $md5 = md5(date("YmdHis"));
