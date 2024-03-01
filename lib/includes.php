516bd5aec384e8e2e4adb9b1d4140e5f956e69b0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-01 22:12:27 +0200

    Atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -3,6 +3,6 @@
     include("../../sis/MysqlConnect.php");
     include("fn.php");
     $con = AppConnect('api_mailgun');
-    // $md5 = md5(date("YmdHis"));
+    $md5 = md5(date("YmdHis"));
 
     $urlPainel = 'http://emails.mohatron.com/';
