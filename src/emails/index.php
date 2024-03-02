c79d13350e0a0d083423fda8bd36446ed7b3b832
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-02 21:45:12 +0200

    Atualizações

--- src/emails/index.php
+++ src/emails/index.php
@@ -1,20 +1,22 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 
-    $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' not null order by codigo desc limit 10";
-    $result = mysqli_query($con, $query);
+echo "TESTE";
 
-    while($d = mysqli_fetch_object($result)){
+    // $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' not null order by codigo desc limit 10";
+    // $result = mysqli_query($con, $query);
 
-        $dados = json_decode($d->mailgun);
+    // while($d = mysqli_fetch_object($result)){
+
+    //     $dados = json_decode($d->mailgun);
         
-        echo $dados->from."<br>";
-        echo $dados->to."<br>";
-        echo $dados->domain."<br>";
-        echo $dados->subject."<br>";
-        // echo $dados->`body-html`."<hr>";
+    //     echo $dados->from."<br>";
+    //     echo $dados->to."<br>";
+    //     echo $dados->domain."<br>";
+    //     echo $dados->subject."<br>";
+    //     echo $dados->`body-html`."<hr>";
 
-        echo "<br><br><br>";
-    }
+    //     echo "<br><br><br>";
+    // }
 
 ?>
