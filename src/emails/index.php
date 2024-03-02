fa8e34ed4dafe19851b67c5abd450fec328403f2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-02 21:46:13 +0200

    Atualizações

--- src/emails/index.php
+++ src/emails/index.php
@@ -1,22 +1,29 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 
-echo "TESTE";
 
-    // $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' not null order by codigo desc limit 10";
-    // $result = mysqli_query($con, $query);
+echo     $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' not null order by codigo desc limit 10";
+    $result = mysqli_query($con, $query);
 
-    // while($d = mysqli_fetch_object($result)){
+    while($d = mysqli_fetch_object($result)){
 
-    //     $dados = json_decode($d->mailgun);
+        $dados = json_decode($d->mailgun);
         
-    //     echo $dados->from."<br>";
-    //     echo $dados->to."<br>";
-    //     echo $dados->domain."<br>";
-    //     echo $dados->subject."<br>";
-    //     echo $dados->`body-html`."<hr>";
+        echo $dados->from."<br>";
+        echo $dados->to."<br>";
+        echo $dados->domain."<br>";
+        echo $dados->subject."<br>";
+        echo $dados->`body-html`."<hr>";
 
-    //     echo "<br><br><br>";
-    // }
+        echo "<br><br><br>";
+    }
 
 ?>
+
+<script>
+    $(function(){
+
+        Carregando('none');
+
+    })
+</script>
