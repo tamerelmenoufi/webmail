1217902602602fa2bdbc4c85a20bdad457106bd5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-02 21:52:55 +0200

    Atualizações

--- src/emails/index.php
+++ src/emails/index.php
@@ -9,11 +9,11 @@
 
         $dados = json_decode($d->mailgun);
         
-        echo $dados['from']."<br>";
-        echo $dados['to']."<br>";
-        echo $dados['domain']."<br>";
-        echo $dados["subject"]."<br>";
-        echo $dados['body-html']."<hr>";
+        // echo $dados['from']."<br>";
+        // echo $dados['to']."<br>";
+        // echo $dados['domain']."<br>";
+        // echo $dados["subject"]."<br>";
+        // echo $dados['body-html']."<hr>";
 
         echo "<br><br><br>";
     }
