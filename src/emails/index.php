99cf1e58c3cb34ce0be62911c8efab8794dffc1d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-31 16:57:01 +0200

    Atualizações

--- src/emails/index.php
+++ src/emails/index.php
@@ -1,30 +1,221 @@
 <?php
-    include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
-
-
-    $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' is not null order by codigo desc limit 10";
-    $result = mysqli_query($con, $query);
-
-    while($d = mysqli_fetch_object($result)){
-
-        $dados = json_decode($d->mailgun);
-        // print_r($dados);
-        $html = "body-html";
-        echo $dados->from."<br>";
-        echo $dados->To."<br>";
-        echo $dados->domain."<br>";
-        echo $dados->subject."<br>";
-        echo $dados->$html."<hr>";
-
-        echo "<br><br><br>";
+        include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
+?>
+<style>
+    .pagina{
+        position:fixed;
+        left:10px;
+        top:60px;
+        bottom:0;
+        right:10px;
+        background-color:#fff;
+        border:solid 0px #333;
+    }
+    .relativo{
+        position:relative;
+        height:100%;
+    }
+    .listaEntradaAcoes{
+        position:absolute;
+        left:0;
+        top:0px;
+        height:80px;
+        right:0;
+        background-color:#eee;
+    }
+    .listaEntrada{
+        position:absolute;
+        left:0;
+        top:80px;
+        bottom:0;
+        right:0;
+        overflow:auto;
+    }
+    .exibeEmail{
+        position:absolute;
+        left:0;
+        top:0px;
+        bottom:0;
+        right:0;
+        background-color:green;
+        overflow:auto;
     }
 
-?>
+    .ItemEmail div h5{
+        font-size:14px;
+        font-weight:bold;
+        color:#a1a1a1;
+        padding:0;
+        margin:0;
+    }
+    .ItemEmail div span{
+        font-size:12px;
+        font-weight:normal;
+        color:#333;
+        padding:0;
+        margin:0;
+    }
+</style>
+<div class="pagina">
+
+
+        <!-- Exibe apenas no PC -->
+        <div class="relativo">
+            <div class="row relativo">
+                <div class="col-md-4 relativo">
+                    <div class="listaEntradaAcoes d-flex flex-column">
+
+
+                        <div style="background-color:#6c757d">
+                            <div class="btn-group btn-group-sm">
+                                <a
+                                    class="btn btn-secondary"
+                                    data-bs-toggle="offcanvas"
+                                    href="#offcanvasDireita"
+                                    role="button"
+                                    aria-controls="offcanvasDireita"
+                                    escrever
+                                >Escrever</a>
+                                <!-- <a
+                                    class="btn btn-primary"
+                                    mover
+                                >Mover</a> -->
+                                <div class="btn-group btn-group-sm" role="group">
+                                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Mover
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Caixa de Entrada</a></li>
+                                    <li><a class="dropdown-item" href="#">Urgentes</a></li>
+                                    <li><a class="dropdown-item" href="#">Empresa A</a></li>
+                                    <li><a class="dropdown-item" href="#">Empresa C</a></li>
+                                    </ul>
+                                </div>
+
+                                <a
+                                    class="btn btn-secondary"
+                                    excluir
+                                >Excluir</a>
+                            </div>
+                        </div>
+
+                        <div class="d-flex flex-row">
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" style="margin-left:0px; margin-top:15px;">
+                                <!-- <label class="form-check-label" for="exampleCheck1">Marcar Todos</label> -->
+                            </div>
+
+
+                            <!-- <div class="input-group input-group-sm ms-3 me-3">
+                                <input type="text" class="form-control form-control-sm" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-sm btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                            </div> -->
+
+                            <div class="input-group input-group-sm mb-3 ms-3 me-1 mt-2">
+                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="listaEntrada">
+                        <ul class="list-group list-group-flush"></ul>
+                    </div>
+                </div>
+                <div class="d-none d-md-block col-md-8 relativo">
+                    <div class="exibeEmail">
+                        <?php
+                        for($i=0;$i<100;$i++){
+                        ?>
+                        Exibição dos e-mails<br>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+</div>
 
 <script>
+
     $(function(){
 
-        Carregando('none');
+        Carregando('none')
 
-    })
+        $.ajax({
+            url:"email/lista.php",
+            success:function(dados){
+                $(".listaEntrada ul").append(dados);
+            }
+        });
+
+
+	 var lastScrollTop = 0, delta = 5;
+	 $(".listaEntrada").scroll(function(){
+		 var nowScrollTop = $(".listaEntrada").scrollTop();
+         var altura = ( $(".listaEntrada ul").outerHeight() - $(".listaEntrada").outerHeight());
+
+        if((nowScrollTop) >= (altura - 10)){
+            console.log(`${nowScrollTop} de ${altura}`)
+            Carregando()
+            $.ajax({
+                url:"email/lista.php",
+                success:function(dados){
+                    $(".listaEntrada ul").append(dados);
+                }
+            });
+
+        }else{
+            // console.log(`Estou fora da área ${nowScrollTop} de ${altura}`);
+        }
+
+		//  if(Math.abs(lastScrollTop - nowScrollTop) >= delta){
+		//  	if (nowScrollTop > lastScrollTop){
+		//  		// ACTION ON
+		//  		// SCROLLING DOWN
+        //         console.log(`DESCE ${nowScrollTop}`)
+		//  	} else {
+		//  		// ACTION ON
+		//  		// SCROLLING UP
+        //          console.log(`SOBE ${nowScrollTop}`)
+		// 	}
+		//  lastScrollTop = nowScrollTop;
+		//  }
+
+	 });
+
+
+     $(document).off('click').on('click','.ItemEmail div i', function(){
+        alert('ação aqui');
+     })
+
+     $("a[escrever]").click(function(){
+        $.alert('Agora vai ser escrito um e-mail');
+     });
+
+     $("a[mover]").click(function(){
+        $.alert('Para qual pasta deseja mover este e-mail?');
+     });
+
+     $("a[excluir]").click(function(){
+        $.confirm({
+            content:"Deseja realmente excluir o e-mail?",
+            title:"Alerta",
+            buttons:{
+                'SIM':function(){
+                    $.alert('E-mail apagado!')
+                },
+                'NÃO':function(){
+
+                }
+            }
+        });
+     });
+ });
+
 </script>
---
+++ src/emails/lista.php
@@ -0,0 +1,60 @@
+<?php
+        include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
+?>
+<style>
+
+</style>
+<?php
+
+
+    $query = "select * from webhook where mailgun->>'$.To' != '' and mailgun->>'$.To' is not null order by codigo desc limit 10";
+    $result = mysqli_query($con, $query);
+
+    while($d = mysqli_fetch_object($result)){
+
+        $dados = json_decode($d->mailgun);
+        // print_r($dados);
+        // $html = "body-html";
+        // echo $dados->from."<br>";
+        // echo $dados->To."<br>";
+        // echo $dados->domain."<br>";
+        // echo $dados->subject."<br>";
+        // echo $dados->$html."<hr>";
+
+        // echo "<br><br><br>";
+
+?>
+<li class="list-group-item">
+
+    <div class="d-flex justify-content-between align-items-center ItemEmail">
+        <div class="p-2">
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <!-- <label class="form-check-label" for="exampleCheck1">Marcar Todos</label> -->
+            </div>
+        </div>
+        <div class="p-2 d-flex flex-column align-items-start flex-grow-1">
+            <h5><?=$dados->from?></h5>
+            <span><?=$dados->subject?></span>
+        </div>
+        <div class="p-2">
+            <i class="fa-solid fa-computer d-none d-md-block"></i>
+            <i class="fa-solid fa-mobile-screen-button d-block d-sm-none"></i>
+        </div>
+    </div>
+
+</li>
+<?php
+}
+?>
+
+<script>
+
+    $(function(){
+        Carregando('none')
+
+    });
+
+</script>
+
+
