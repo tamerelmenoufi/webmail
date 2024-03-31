f30938fd1fddd0852ebd35c45151800f97f3128e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-31 17:53:14 +0200

    Atualizações

--- src/emails/email.php
+++ src/emails/email.php
@@ -1,7 +1,7 @@
 <?php
         include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 
-    $query = "select * from webhook where codigo = '{$_GET['codigo']}'";
+    echo $query = "select * from webhook where codigo = '{$_GET['codigo']}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
