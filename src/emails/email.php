7ada4bc72e32a452923f1e785d56d5e66e70c008
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-19 01:56:27 +0200

    Atualizações

--- src/emails/email.php
+++ src/emails/email.php
@@ -5,7 +5,7 @@
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
-    $dados = json_decode(file_get_contents("/var/www/html/mailgun/emails/{$d->codigo}/dados.json"));
+    $dados = file_get_contents("http://mailgun.mohatron.com/emails/{$d->codigo}/dados.json");
 
     $html = "body-html";
     echo $dados->$html;
