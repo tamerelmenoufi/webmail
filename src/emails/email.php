3e1fab3d38ab3a53101490f2ad65edf6b7aba23b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-19 03:31:34 +0200

    Atualizações

--- src/emails/email.php
+++ src/emails/email.php
@@ -1,21 +1,12 @@
 <?php
-
-    include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
+        include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 
     $query = "select * from webhook where codigo = '{$_GET['codigo']}'";
     $result = mysqli_query($con, $query);
     $d = mysqli_fetch_object($result);
 
-    $d1 = file_get_contents("http://mailgun.mohatron.com/emails/{$d->codigo}/dados.json");
-
-    $dados = json_decode($d1);
-
-    $cid = "content-id-map";
-    $img_corpo = json_decode($dados->$cid);
-    print_r($img_corpo);
-
+    $dados = json_decode(file_get_contents("http://mailgun.mohatron.com/emails/{$d->codigo}/dados.json"));
 
     $html = "body-html";
     echo $dados->$html;
 
-?>
